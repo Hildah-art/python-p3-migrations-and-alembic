7d8d48c265cb7fa527afb0bba2083852885523ae
Hildah-art <njagahildah@gmail.com>
2025-05-30 13:28:28 +0300

    learnt migrations and alembic

---
+++ app/migrations/versions/ce0f7503def7_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: ce0f7503def7
+Revises: 
+Create Date: 2025-05-22 10:59:40.961778
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ce0f7503def7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
